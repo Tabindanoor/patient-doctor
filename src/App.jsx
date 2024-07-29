9bee45c26f12e2934ee2fd25c6a5b064cbcd6bd8
Tabindanoor <tabindanoor415@gmail.com>
2024-07-29 22:47:12 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import logo from '../src/assets/EasyReport.png'
+import Login from './Login'
+const App = () => {
+  return (
+    <div  className='bg-[#E5E5E5] '>
+        <Login/>
+    </div>
+  )
+}
+
+export default App
